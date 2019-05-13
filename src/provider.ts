ac48511335aa96e20fdd87e65f84b9a10de9c63e
Joshua Thome <joshpthome@gmail.com>
2019-05-12 20:08:49 -0500

    Syntax highlighting

--- src/provider.ts
+++ src/provider.ts
@@ -17,48 +17,62 @@ export default class Provider implements v.CompletionItemProvider, v.HoverProvid
     public provideCompletionItems(document: v.TextDocument, position: v.Position, token: v.CancellationToken, context: v.CompletionContext): v.ProviderResult<v.CompletionItem[] | v.CompletionList> {
         throw new Error('Method not implemented.')
     }
+
     public provideHover(document: v.TextDocument, position: v.Position, token: v.CancellationToken): v.ProviderResult<v.Hover> {
         throw new Error('Method not implemented.')
     }
+
     public provideSignatureHelp(document: v.TextDocument, position: v.Position, token: v.CancellationToken, context: v.SignatureHelpContext): v.ProviderResult<v.SignatureHelp> {
         throw new Error('Method not implemented.')
     }
+
     public provideDefinition(document: v.TextDocument, position: v.Position, token: v.CancellationToken): v.ProviderResult<v.Location | v.Location[] | v.LocationLink[]> {
         throw new Error('Method not implemented.')
     }
+
     public provideReferences(document: v.TextDocument, position: v.Position, context: v.ReferenceContext, token: v.CancellationToken): v.ProviderResult<v.Location[]> {
         throw new Error('Method not implemented.')
     }
+
     public provideDocumentHighlights(document: v.TextDocument, position: v.Position, token: v.CancellationToken): v.ProviderResult<v.DocumentHighlight[]> {
         throw new Error('Method not implemented.')
     }
+
     public provideDocumentSymbols(document: v.TextDocument, token: v.CancellationToken): v.ProviderResult<v.SymbolInformation[] | v.DocumentSymbol[]> {
         throw new Error('Method not implemented.')
     }
+
     public provideWorkspaceSymbols(query: string, token: v.CancellationToken): v.ProviderResult<v.SymbolInformation[]> {
         throw new Error('Method not implemented.')
     }
+
     public provideCodeActions(document: v.TextDocument, range: v.Range | v.Selection, context: v.CodeActionContext, token: v.CancellationToken): v.ProviderResult<(v.Command | v.CodeAction)[]> {
         throw new Error('Method not implemented.')
     }
+
     public provideCodeLenses(document: v.TextDocument, token: v.CancellationToken): v.ProviderResult<v.CodeLens[]> {
         throw new Error('Method not implemented.')
     }
+
     public provideDocumentColors(document: v.TextDocument, token: v.CancellationToken): v.ProviderResult<v.ColorInformation[]> {
         throw new Error('Method not implemented.')
     }
     public provideColorPresentations(color: v.Color, context: { document: v.TextDocument, range: v.Range }, token: v.CancellationToken): v.ProviderResult<v.ColorPresentation[]> {
         throw new Error('Method not implemented.')
     }
+
     public provideDocumentFormattingEdits(document: v.TextDocument, options: v.FormattingOptions, token: v.CancellationToken): v.ProviderResult<v.TextEdit[]> {
         throw new Error('Method not implemented.')
     }
+
     public provideDocumentRangeFormattingEdits(document: v.TextDocument, range: v.Range, options: v.FormattingOptions, token: v.CancellationToken): v.ProviderResult<v.TextEdit[]> {
         throw new Error('Method not implemented.')
     }
+
     public provideOnTypeFormattingEdits(document: v.TextDocument, position: v.Position, ch: string, options: v.FormattingOptions, token: v.CancellationToken): v.ProviderResult<v.TextEdit[]> {
         throw new Error('Method not implemented.')
     }
+
     public provideRenameEdits(document: v.TextDocument, position: v.Position, newName: string, token: v.CancellationToken): v.ProviderResult<v.WorkspaceEdit> {
         throw new Error('Method not implemented.')
     }
